ae83a4d487f6ed0e2b34314fb22b5883c0a7d55a
Aleksandr Schemelev <prophreaker@gmail.com>
2023-02-19 15:30:23 +0300

    feat: add solution for 1792C Min Max Sort

--- C1792/main.rs
+++ C1792/main.rs
@@ -7,20 +7,38 @@ fn main() {
     stdin.read_line(&mut t).expect("String");
 
     let t = t.trim().parse::<u16>().expect("Number");
-    let mut m = String::new();
-    let mut input_data: Vec<Vec<u32>> = Vec::new();
-    let mut nums = String::new();
+    let mut l = String::new();
+    let mut p = String::new();
 
     for _ in 0..t {
-        stdin.read_line(&mut m).expect("Number of test cases");
-        stdin.read_line(&mut nums).expect("String of numbers");
+        stdin.read_line(&mut l).expect("Length of the permutation");
+        let len = l.trim().parse::<usize>().expect("Number");
 
-        let mut arr: Vec<u32> = Vec::new();
+        stdin.read_line(&mut p).expect("Permutation");
 
-        nums.split_whitespace()
-            .for_each(|x| arr.push(x.parse::<u32>().expect("Number")));
+        let mut pos: Vec<usize> = vec![0; len];
 
-        nums = String::from("");
-        input_data.push(arr);
+        // WE DON'T REALLY NEED THE PERMUTATION ITSELF, WE NEED ONLY THE POSITION OF IT'S ELEMENTS,
+        // SO WE BUILD THE POSITION VECTOR INSTEAD
+        p.split_whitespace()
+            .enumerate()
+            .for_each(|(i, x)| pos[(x.parse::<usize>().expect("Number")) - 1] = i);
+
+        // MAX NEEDED SWAPS
+        let mut n = pos.len() / 2;
+
+        while n > 0 {
+            if pos[n - 1] < pos[n] && pos[len - n - 1] < pos[len - n] {
+                n -= 1;
+                continue;
+            }
+
+            break;
+        }
+
+        l = String::from("");
+        p = String::from("");
+
+        println!("{n}");
     }
 }
