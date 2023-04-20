f430bbccbc9f89fdeb5789e07b55feeb8081af98
Aleksandr Schemelev <prophreaker@gmail.com>
2023-04-20 22:03:36 +0300

    feat: implement 1814c task

--- C1814/README.md
+++ C1814/README.md
@@ -1,1 +1,1 @@
-[C. Search in Parallel](https://codeforces.com/problemset/problem/1814/C)
+### [C. Search in Parallel](https://codeforces.com/problemset/problem/1814/C)
--- C1814/index.ts
+++ C1814/index.ts
@@ -17,7 +17,37 @@ const main = async () => {
     });
   });
 
-  const tests = inputData.slice(1);
+  const [testsNum] = inputData.splice(0, 1);
+
+  for (const _ of Array(+testsNum)) {
+    const [_, s1, s2] = inputData.splice(0, 1)[0].split(' ').map(Number);
+
+    const r = inputData.splice(0, 1)[0].split(' ').map(Number);
+
+    let l1 = [];
+    let l2 = [];
+
+    let t1 = s1;
+    let t2 = s2;
+
+    const sortedR = [...r].sort((a, b) => b - a);
+    const map: Record<string, any> = {};
+
+    for (let [key, el] of Object.entries(r)) {
+      map[el] ??= [];
+      map[el].push(+key + 1);
+    }
+
+    for (const el of sortedR) {
+      const idx = map[el].shift();
+      const b = idx;
+
+      t1 > t2 ? (l2.push(b), (t2 += s2)) : (l1.push(b), (t1 += s1));
+    }
+
+    console.log(`${l1.length} ${l1.join(' ')}`);
+    console.log(`${l2.length} ${l2.join(' ')}`);
+  }
 };
 
 main();
