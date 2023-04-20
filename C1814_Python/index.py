eb8a2ad284c15c18f03efc47c8142b79605ae803
Aleksandr Schemelev <prophreaker@gmail.com>
2023-04-21 00:29:44 +0300

    solution: 1814c in Python

--- C1814_Python/index.py
+++ C1814_Python/index.py
@@ -1,21 +1,29 @@
 import sys
+from collections import deque
 
 t, *data = map(str.split, sys.stdin.read().splitlines())
 
 for i in range(int(t[0])):
-    _, s1, s2 = map(int, data.pop(0))
-    r = list(map(int, data.pop(0)))
+    _, s1, s2 = map(int, data[i*2])
+    r = list(map(int, data[i*2+1]))
     r_sorted = sorted(r, reverse=True)
-    l1, l2 = [], []
+    l1, l2 = deque(), deque()
     t1, t2 = s1, s2
-    m = {el: [i+1 for i, x in enumerate(r) if x == el] for el in r}
+    m = {}
+
+    for i, el in enumerate(r):
+        if el in m:
+            m[el].append(i+1)
+        else:
+            m[el] = [i+1]
 
     for el in r_sorted:
         b = m[el].pop(0)
-        (l2 if t1 > t2 else l1).append(b)
         if t1 > t2:
+            l2.append(b)
             t2 += s2
         else:
+            l1.append(b)
             t1 += s1
     print(len(l1), ' '.join(map(str, l1)))
     print(len(l2), ' '.join(map(str, l2)))
